ba0e4ef5f526f2277388e8bf6f6b000a0a55c70d
Randy Coburn <morfien101@gmail.com>
2018-09-17 11:19:22 +0200

    Updating the metadata file to reduce foodcritic errors

--- metadata.rb
+++ metadata.rb
@@ -7,32 +7,16 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
 
 source_url 'https://github.com/morfien101/chef-waiter-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/morfien101/chef-waiter-cookbook/issues' if respond_to?(:issues_url)
+# This is the lowest version that I have tested it on.
+chef_version '=> 12.9.41'
 
-{
-  'ubuntu' => '18.04',
-  'centos' => ['6', '7'],
-  'amazonlinux' =>  '',
-  'windows' => '2012',
-}.each do |os, version|
-  def support(o, v)
-    supports o, "= #{v}"
-  end
-
-  if version.is_a? Array
-    version.each do |v|
-      support os, v
-    end
-
-    break
-  end
-
-  if version.empty?
-    support os
-    break
-  end
-
-  support os, version
-end
+# Very likely to support many more but this is what I have tested it with
+supports 'ubuntu', '~> 18.04'
+supports 'centos', '~> 6'
+supports 'centos', '~> 7'
+supports 'amazon'
+supports 'windows', '=> 2012'
 
 depends 'iptables'
 depends 'zipfile', '~> 0.2.0'
