d224374dcf7c5f5ec86bd38c335d2042ef01b955
Randy Coburn <morfien101@gmail.com>
2018-11-21 15:48:27 +0200

    bumping version and updating tests for new version

--- test/integration/linux/serverspec/service_spec.rb
+++ test/integration/linux/serverspec/service_spec.rb
@@ -6,7 +6,7 @@ describe service('chefwaiter') do
 end unless upstart_platform?
 
 describe command('cat /etc/init/chefwaiter.conf') do
-  its(:stdout) { should match /runlevel \[\d*2\d*\]/ }
+  its(:stdout) { should match /start on stopped rc/ }
 end if upstart_platform?
 
 describe command('sudo status chefwaiter') do
@@ -17,3 +17,7 @@ describe file('/etc/chefwaiter/config.json') do
   it { should be_file }
   its(:content) { should match /"state_table_size": 40,/ }
 end
+
+describe command('curl localhost:8901/status') {
+  its(:stdout) { should match /"healthy": true/ }
+}
